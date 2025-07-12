1b54f7d5071021c38ca0cb7484b2086bf0a6ea6b
Bernie Elbourn <benri421@gmail.com>
2025-07-12 14:43:44 +0100

    v1.3.2 updated for api 36

---
+++ app/src/main/java/com/elbourn/android/btsetup/MainActivity.java
@@ -0,0 +1,26 @@
+package com.elbourn.android.btsetup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
+        finishAffinity();
+    }
+}
